978fe3d07e31d8a8098545771e641b42646f0207
Luis Marte <luisdavidmarte00@gmail.com>
2024-11-01 20:13:47 -0400

    Added .Web solution that its from the user web CRUD

---
+++ Taller.Web/Models/AspCrudContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taller.Web.Models;
+
+public partial class AspCrudContext : DbContext
+{
+    public AspCrudContext()
+    {
+    }
+
+    public AspCrudContext(DbContextOptions<AspCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Nota> Notas { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Nota>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Title)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.User).WithMany()
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Notas__user_id__48CFD27E");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3213E83F9ABF6638");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Password)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
